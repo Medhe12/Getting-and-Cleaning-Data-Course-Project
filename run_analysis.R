ef0dd976e06c3d06403896fb66cdda27e7ea1173
Medha Sawhney <sawhney.medha@gmail.com>
2019-03-28 20:15:46 +0530

    Edited code for project script to clean data

--- run_analysis.R
+++ run_analysis.R
@@ -1,26 +1,26 @@
-library(dplyr)
-library(plyr) # load plyr first, then dplyr 
-library(data.table) # a prockage that handles dataframe better
-library(dplyr) # for fancy data table manipulations and organization
+# Loading the required packages
+library(plyr) # plyr is loaded before dplyr for table manipulations
+library(data.table) # to handle data frames faster
+library(dplyr) # to manipulate and organise data tables
 
-if (!getwd() == "./out-of-box-samples") {
-  dir.create("./out-of-box-samples")
-}
+# set the required appropriated working directory for the script
+setwd("D:/DataScience_JHU/Practice/GettingCleaningDataProgAss")
+dir <- getwd() # storing the path in a dir variable for easy download
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip" #storing the given data url in variable url
 
-path <- getwd()
-url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(url, file.path(path, "dataFiles.zip"))
-unzip(zipfile = "dataFiles.zip")
+# using the download.file function to download the data from the given url in .zip format at a required destination
+download.file(url, file.path(dir, "dataFiles.zip")) 
+unzip(zipfile = "dataFiles.zip") # unzipping the downladed data from the zip file
 
 # read train data
-X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
-Y_train <- read.table("./UCI HAR Dataset/train/Y_train.txt")
-Sub_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+Train_X <- read.table("./UCI HAR Dataset/train/Train_X.txt")
+Train_Y <- read.table("./UCI HAR Dataset/train/Train_Y.txt")
+Train_Sub <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 
 # read test data
-X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
-Y_test <- read.table("./UCI HAR Dataset/test/Y_test.txt")
-Sub_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+Test_X <- read.table("./UCI HAR Dataset/test/Test_X.txt")
+Test_Y <- read.table("./UCI HAR Dataset/test/Test_Y.txt")
+Test_Sub <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 
 # read data description
 variable_names <- read.table("./UCI HAR Dataset/features.txt")
@@ -29,25 +29,25 @@ variable_names <- read.table("./UCI HAR Dataset/features.txt")
 activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 
 # 1. Merges the training and the test sets to create one data set.
-X_total <- rbind(X_train, X_test)
-Y_total <- rbind(Y_train, Y_test)
-Sub_total <- rbind(Sub_train, Sub_test)
+X_merged <- rbind(Train_X, Test_X)
+Y_merged <- rbind(Train_Y, Test_Y)
+Sub_merged <- rbind(Train_Sub, Test_Sub)
 
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-selected_var <- variable_names[grep("mean\\(\\)|std\\(\\)",variable_names[,2]),]
-X_total <- X_total[,selected_var[,1]]
+mean_std_vars <- variable_names[grep("mean\\(\\)|std\\(\\)",variable_names[,2]),]
+X_merged <- X_merged[,mean_std_vars[,1]]
 
 # 3. Uses descriptive activity names to name the activities in the data set
-colnames(Y_total) <- "activity"
-Y_total$activitylabel <- factor(Y_total$activity, labels = as.character(activity_labels[,2]))
-activitylabel <- Y_total[,-1]
+colnames(Y_merged) <- "activity"
+Y_merged$activitylabel <- factor(Y_merged$activity, labels = as.character(activity_labels[,2]))
+activitylabel <- Y_merged[,-1]
 
 # 4. Appropriately labels the data set with descriptive variable names.
-colnames(X_total) <- variable_names[selected_var[,1],2]
+colnames(X_merged) <- variable_names[mean_std_vars[,1],2]
 
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average
 # of each variable for each activity and each subject.
-colnames(Sub_total) <- "subject"
-total <- cbind(X_total, activitylabel, Sub_total)
+colnames(Sub_merged) <- "subject"
+total <- cbind(X_merged, activitylabel, Sub_merged)
 total_mean <- total %>% group_by(activitylabel, subject) %>% summarize_each(funs(mean))
 write.table(total_mean, file = "./UCI HAR Dataset/tidydata.txt", row.names = FALSE, col.names = TRUE)
